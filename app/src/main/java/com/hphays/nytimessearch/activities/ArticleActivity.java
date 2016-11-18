70a408fe11aa8bfc317beb2def2e753edff685af
Hunter Hays <hhays@yahoo-inc.com>
2016-11-17 16:12:53 -0800

    Article sharing added

--- app/src/main/java/com/hphays/nytimessearch/activities/ArticleActivity.java
+++ app/src/main/java/com/hphays/nytimessearch/activities/ArticleActivity.java
@@ -1,13 +1,19 @@
 package com.hphays.nytimessearch.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.support.v7.widget.ShareActionProvider;
 
 import com.hphays.nytimessearch.Article;
 import com.hphays.nytimessearch.R;
@@ -34,8 +40,32 @@ public class ArticleActivity extends AppCompatActivity {
         });
         webView.loadUrl(url.getWebUrl());
 
-            }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_article, menu);
+
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        ShareActionProvider miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+
+        // get reference to WebView
+        WebView wvArticle = (WebView) findViewById(R.id.wvArticle);
+
+        // pass in the URL currently being used by the WebView
+        shareIntent.putExtra(Intent.EXTRA_TEXT, wvArticle.getUrl());
+        miShareAction.setShareIntent(shareIntent);
+
+        return true;
 
     }
+
+}
 
 
